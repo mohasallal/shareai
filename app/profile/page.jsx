b28500139c54afc73e21dd0473ffdbd3421be3e3
Mohammad Alsallal <malslal787@gmail.com>
2024-08-15 08:05:52 +0300

    Crud is done

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -28,7 +28,23 @@ const MyProfile = () => {
   };
 
   const handleDelete = async (post) => {
-    
+    const hasConfirmed = confirm(
+      "Are you sure you want to Delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   };
 
   return (
